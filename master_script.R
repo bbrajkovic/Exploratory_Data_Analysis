c826e5427937721e3240a5bf9e3aa11f5c5b6e7c
Home Office <homeoffice@Bojans-MacBook-Pro.local>
2018-06-17 16:56:52 +0100

    2 chart scripts created, added master script

---
+++ master_script.R
@@ -0,0 +1,5 @@
+## Master script ##
+
+source("data_loading_script.R")
+source("plot1.R")
+source("plot2.R")
---
+++ plot2.R
@@ -0,0 +1,5 @@
+## SECOND CHART CODE ##
+
+plot(data$Global_active_power~data$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
